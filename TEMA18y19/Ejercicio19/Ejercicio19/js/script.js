a3e9e88939f0cd316637e5ddd614e481bcdf35f9
A-Algaba <aac0077@alu.medac.es>
2026-05-12 11:38:38 +0200

    update script js

--- TEMA18y19/Ejercicio19/Ejercicio19/js/script.js
+++ TEMA18y19/Ejercicio19/Ejercicio19/js/script.js
@@ -1,31 +1,36 @@
 function addUser() {
-    let username= document.getElementById("username").value;
-    let passwd1 = document.getElementById("password").value;
-    let passwd2 = document.getElementById("confirmPassword").value;
-    let usernameColor= document.getElementById("username");
-    let passwd1Color = document.getElementById("password");
-    let passwd2Color = document.getElementById("confirmPassword");
+    let username = document.getElementById("username");
+    let passwd1 = document.getElementById("password");
+    let passwd2 = document.getElementById("confirmPassword");
     let usuariosRegistrados = document.getElementById("registeredUsers");
 
-    if (passwd1 == false && username == false && passwd2 == false) {
+    if (passwd1.value == false || username.value == false || passwd2.value == false) {
 
-        alert("Escribe algo");
+        alert("Te faltan datos");
 
     } else {
 
-        if(passwd1 == passwd2) {
+        if(passwd1.value == passwd2.value) {
             let nuevoHijo = document.createElement("p");
-            nuevoHijo.textContent = "Usuario: " + username + " Contraseña: " + passwd2;
+            nuevoHijo.textContent = "Usuario: " + username.value + " Contraseña: " + passwd2.value;
 
             usuariosRegistrados.appendChild(nuevoHijo);
         } else {
-                passwd1Color.style.backgroundColor  = "red";
-                passwd2Color.style.backgroundColor = "red";
+                passwd1.style.backgroundColor  = "red";
+                passwd2.style.backgroundColor = "red";
+
+                passwd1.value = "Las contraseñas no coinciden";
+                passwd2.value = "Las contraseñas no coinciden";
+
+                passwd1.type = "text";
+                passwd2.type = "text";
         }
         
         //IMPORTANTE
         //Con value, se obtiene el valor de un elemento html (en este caso el texto), y sin value, 
         //es el elemento en si para poder aplicar color por ejemplo
+
+        //SUPER IMPORTANTE QUE SI VAMOS A COMPARAR VALORES ESCRITOS, PONER .value PORQUE SINO, ESTARA COMPRARANDO LOS ELEMENTOS, NO EL VALOR ESCRITO
 
         //Con text content se añade contenido a una variable
         //con appendchild se añade un elemento hijo si hay un elemento padre
